41ea594a1bb1d8840b7077ca1cdd27e90aa30986
Mohit Gupta <moodiemohit@gmail.com>
2022-05-10 04:04:02 +0530

    Now getting NIfty price from aastha console

--- auto_trade_functions.py
+++ auto_trade_functions.py
@@ -9,6 +9,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from datetime import datetime as dt
 from datetime import timedelta as td
+from bs4 import BeautifulSoup
+import re
 
 
 logger = logging.getLogger(__name__)
@@ -33,7 +35,7 @@ def start_aastha():
         return driver
 
 
-def login(driver,id,password):
+def login(driver,id,passw):
     wait = WebDriverWait(driver,10)
     try:
         user_id = wait.until(
@@ -43,27 +45,42 @@ def login(driver,id,password):
         password = wait.until(
         EC.presence_of_element_located((
         By.XPATH,'/html/body/app-root/app-login/div/div/div[3]/div/div[3]/ul/li[2]/div[2]/app-loginwidget/div/form/div/div/div[6]/div/mat-form-field/div/div[1]/div[3]/input')))
+
     except:
-        logger.Error('Could not find the element either for id or password')
+        logger.error('Could not find the element either for id or password')
 
     else:
-        login = driver.find_element_by_xpath('/html/body/app-root/app-login/div/div/div[3]/div/div[3]/ul/li[2]/div[2]/app-loginwidget/div/form/div/div/div[10]/div/button')
         user_id.send_keys(id)
-        password.send_keys(password)
+        password.send_keys(passw)
+        login = wait.until(
+        EC.presence_of_element_located((
+        By.XPATH,'/html/body/app-root/app-login/div/div/div[3]/div/div[3]/ul/li[2]/div[2]/app-loginwidget/div/form/div/div/div[10]/div/button')))
         login.click()
         logger.info('Logged in Sucessfully')
         return driver
 
 def get_nifty_price(driver):
-    pass
-    return [driver,nifty_price]
+    wait = WebDriverWait(driver,10)
+    try:
+        index = 'nifty'
+        while not bool(re.findall(r'\d{5}',index)):
+            index = wait.until(
+            EC.presence_of_element_located((
+            By.CLASS_NAME,'lestSection'))).text
+    except:
+        logger.error('Could Not Fetch Nifty Price')
+    else:
+        logger.info('Fetched Nifty Price Successfully')
+        return [driver,re.findall(r'\d{5}',index)[0]]
 
 def get_nifty_last_30days():
     try:
-        nifty_data = yf.download('^NSEI',period='30d')
+        nifty_data = yf.download('^NSEI',period='31d')
+        nifty_data = nifty_data[:-1]
     except:
         logger.error('Could not get data of nifty for last 30 days from yahoo finance')
     else:
+        looger.info('Fetched Last 30 Days Data From Yahoo Finance')
         return nifty_data
 def days_to_expiry(nifty_data):
     date = nifty_data.tail(1).index[0]
@@ -75,8 +92,7 @@ def days_to_expiry(nifty_data):
             delta = thurs - today
             return [thurs,delta.days]
 
-def get_prob_of_each_strike(nifty_data,strike_price,days):
-
+def get_prob_of_each_strike(nifty_data,strike_price,days,nifty_prob_39days):
     #adding row into nifty_data(last 30 days)
     last_date = nifty_data.index[-1]
     target_date = last_date+td(days=days)
@@ -93,11 +109,12 @@ def get_prob_of_each_strike(nifty_data,strike_price,days):
     nifty_data.dropna(axis=0,inplace=True)
 
     nifty_data['days'] = range(days,days+30)
-    nifty_data['prob'] = nifty_data.apply(lambda x : check_prob(int(x['days']),x['max_perct']),axis=1)
+    nifty_data['prob'] = nifty_data.apply(lambda x : get_probability(float(x['max_perct']),nifty_prob_39days[nifty_prob_39days['day']==int(x['days'])]),axis=1)
 
-
+    print(nifty_data)
     # getting info into dict format to process it
     row = nifty_data[nifty_data['prob']==nifty_data['prob'].max()]
+    print('Row : ',row)
     max_perct = row['max_perct'].values[0]
     max_days = row['days'].values[0]
     prob = row['prob'].values[0]
@@ -105,20 +122,24 @@ def get_prob_of_each_strike(nifty_data,strike_price,days):
         trend='Postive'
     else:
         trend='Negative'
-    print(f'Strike Price : {strike_price}, Prob is {prob}')
+
+
+    print(f'Strike Price : {strike_price}, Prob is {prob},max days are {max_days}, max perct : {max_perct}')
 
 
 def look_for_best_strikeprice(nifty_price,nifty_data,days):
+    print(dt.today())
     strike_dic = {}
     nifty_price = (round(float(nifty_price)/50))*50
-    for strike_price in range(nifty_price-1000,nifty_price+1000,50):
-        get_prob_of_each_strike(nifty_data,strike_price,days)
+    nifty_prob_39days = concat_eachday_maxmoves()
+    for strike in range(nifty_price-1000,nifty_price+1000,50):
+        get_prob_of_each_strike(nifty_data,strike,price,days,nifty_prob_39days)
 
-    return strike_dic
+    print(dt.today())
 
 
-def check_prob(days,maximum):
-    nifty_prob = pd.read_csv('NIfty_prob_data\{}_day.csv'.format(days))
+
+def get_probability(maximum,nifty_prob):
     if maximum > 0:
         times = len(nifty_prob[(nifty_prob['perct'] > maximum)])
     elif maximum < 0:
@@ -126,5 +147,19 @@ def check_prob(days,maximum):
     total = len(nifty_prob)
     return 100-((times/total)*100)
 
-nifty_data = get_nifty_last_30days()
-print(look_for_best_strikeprice(16411,nifty_data,4))
+
+
+
+def concat_eachday_maxmoves():
+    nifty_prob_39days = pd.DataFrame()
+    for day in range(1,40):
+        nifty_prob =  pd.read_csv(f'NIfty_prob_data/{day}_day.csv',index_col='Date')
+        nifty_prob['day'] = day
+        nifty_prob_39days = pd.concat([nifty_prob_39days,nifty_prob])
+        nifty_prob_39days.dropna(axis=0,inplace=True)
+    return nifty_prob_39days
+
+
+driver = start_aastha()
+driver = login(driver,'AF5325','lIVINGLIFE@1')
+get_nifty_price(driver)
